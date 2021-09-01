f00dfd6baa6abf1d6327873ba638b97ac39c6888
dmcgriffy <david@mcgriffy.com>
2021-09-01 15:40:39 -0500

    debugs

--- fetch-release.ps1
+++ fetch-release.ps1
@@ -38,6 +38,7 @@ $API_URL="https://api.github.com/repos/$REPO"
 if ($TOKEN -ne $null) {
     $HEADER="-H ""Authorization: token $TOKEN"""
 }
+echo "Header: $HEADER"
 $RELEASE_DATA=$(curl $HEADER "$API_URL/releases/$Env:INPUT_VERSION")
 echo $RELEASE_DATA
 $MESSAGE=$(echo "$RELEASE_DATA" | & "$Env:GITHUB_ACTION_PATH\bin\jq-win64.exe" -r ".message")
