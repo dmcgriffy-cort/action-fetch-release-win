884484d8a044ac7b23f4b75076dda2f070e8849c
dmcgriffy <david@mcgriffy.com>
2021-09-01 13:39:38 -0500

    Add vars and try the 1st curl

--- fetch-release.ps1
+++ fetch-release.ps1
@@ -4,6 +4,35 @@ if ($Env:INPUT_FILE -eq $null) {
     exit 1
 }
 
-echo "Input file: " $Env:INPUT_FILE
+if ($Env:GITHUB_REPOSITORY -eq $null) {
+  echo "Missing GITHUB_REPOSITORY env variable"
+  exit 1
+}
+
+REPO=$Env:GITHUB_REPOSITORY
+if ($Env:INPUT_REPO -ne $null) {
+  REPO=$INPUT_REPO
+}
+
+# Optional target file path
+TARGET=$Env:INPUT_FILE
+if ($Env:INPUT_TARGET -ne $null) {
+  TARGET=$Env:INPUT_TARGET
+}
+
+# Optional personal access token for external repository
+TOKEN=$Env:GITHUB_TOKEN
+if ($Env:INPUT_TOKEN -ne $null) {
+  TOKEN=$INPUT_TOKEN
+}
+
+echo "Input file: $Env:INPUT_FILE"
+
+API_URL="https://api.github.com/repos/$REPO"
+RELEASE_DATA=$(curl $TOKEN -ne $null ? "-H ""Authorization: token $TOKEN""" \
+                    "$API_URL/releases/$Env:INPUT_VERSION")
+MESSAGE=$(echo "$RELEASE_DATA" | jq -r ".message")
+
+echo "Message $MESSAGE"
 
 exit 0
